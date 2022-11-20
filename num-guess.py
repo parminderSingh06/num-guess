47a8d93738fc708f2914651d129af4938528e6aa
Parminder Singh <parmindersingh1086@gmail.com>
2022-11-19 17:34:11 -0800

    Added Full Game
    completed the game code using play function and a dictionary
    to allow players to choose difficulty. Game now has full function.

--- num-guess.py
+++ num-guess.py
@@ -1,3 +1,17 @@
-print("Hello welcome to the Number Guessing game!")
-print("This game will select a random number from 1-100 and you have to guess the number.(Hints will be provided)")
+import random
+
+def main():
+    print("Hello welcome to the Number Guessing game!")
+    print("This game will select a random number from 1-100 and you have to guess the number.(Hints will be provided)")
+    print(generate())
+
+def generate():
+    x = random.uniform(0,100)
+    return x
+
+if __name__ == "__main__":
+    main()
+
+
+
 
